637f40003b6dae5dd16a63574eca30e9c06ff4fb
mreed5812 <mreed5812@gmail.com>
2019-09-22 21:47:28 -0500

    created battleship objects and divided game board

--- app.js
+++ app.js
@@ -1,69 +1,75 @@
 
-//setup
-const gridContainer = document.querySelector('.grid-container');
-const GridSide = 10;      // initial gridSide is 4, set the grid attribute to repeat 4x4
-gridContainer.setAttribute('style', 'grid: repeat(' + GridSide + ',auto) / repeat(' + GridSide + ',auto)');
-const GridTotal = GridSide * GridSide;
+window.onload = function () {
 
-document.addEventListener("DOMContentLoaded", function () {
+    //setup
+    var gridContainer = document.querySelectorAll(".grid-container");
+    var battleships = [];
+    const GridSide = 10;
+    const GridTotal = GridSide * GridSide;
+
+
     drawGrid(GridSide, GridTotal);
     createShips();
-});
 
-function BattleShip(length) {
-    this.length = length;
-    this.name = length.toString();
-}
-
-var battleships = [];
-
-function createShips() {
-    for (i = 1; i < 6; i++) {
-        let ship = new BattleShip(i);
-        battleships.push(ship)
+    function drawGrid(gridSide, gridTotal) {
+        for (i = 0; i < gridContainer.length; i++) {
+            for (j = 0; j < gridTotal; j++) {
+                div = document.createElement('div')
+                div.setAttribute('id', 'gridBox')
+                gridContainer[i].appendChild(div);
+            }
+            gridContainer[i].setAttribute('style', 'grid: repeat(' + gridSide + ',auto) / repeat(' + gridSide + ',auto)')
+        }
     }
-}
 
-
-function testShipObject() {
-    for (i = 0; i < battleships.length; i++) {
-        console.log(battleships[i]);
+    function BattleShip(length) {
+        this.length = length;
+        this.name = length.toString();
+        this.hits = 0;
+        this.isAlive = true;
     }
-}
 
-var resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', function () {
-    testShipObject();
-});
-
-function drawGrid(gridSide, gridTotal) {
-    for (i = 0; i < gridTotal; i++) {
-        div = document.createElement('div')
-        div.setAttribute('id', 'gridBox')
-        gridContainer.appendChild(div);
+    //create ship and add to battleships array
+    function createShips() {
+        for (i = 1; i < 6; i++) {
+            let ship = new BattleShip(i);
+            battleships.push(ship)
+        }
     }
-    gridContainer.setAttribute('style', 'grid: repeat(' + gridSide + ',auto) / repeat(' + gridSide + ',auto)')
-}
 
-function markSquare() {
-    var divs = document.querySelectorAll('.grid-container div');
-    divs.forEach((div) => {
-        div.addEventListener('mouseover', function (e) {
-            e.target.setAttribute('style', 'background: black');
+    function testShipObject() {
+        for (i = 0; i < battleships.length; i++) {
+            console.log(battleships[i]);
+        }
+    }
+
+    var resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', function () {
+        testShipObject();
+    });
+
+    function markSquare() {
+        var divs = document.querySelectorAll('.grid-container div');
+        divs.forEach((div) => {
+            div.addEventListener('mouseover', function (e) {
+                e.target.setAttribute('style', 'background: black');
+            });
         });
+    }
+
+    function gameSetup() {
+        var divs = document.querySelectorAll('.grid-container div')
+        divs.forEach((div) => {
+            div.setAttribute('style', 'background: white');
+        });
+    }
+
+    var resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', function () {
+        //gameSetup();
     });
 }
 
-function gameSetup() {
-    var divs = document.querySelectorAll('.grid-container div')
-    divs.forEach((div) => {
-        div.setAttribute('style', 'background: white');
-    });
-}
 
-var resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', function () {
-    //gameSetup();
-});
 
 
