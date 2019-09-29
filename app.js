d2cdebed34fa8a683ff8900ed13f317c474a67ca
mreed5812 <mreed5812@gmail.com>
2019-09-29 10:29:18 -0500

    organized code and added ship placement logic for neighboring cells

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
-
 window.onload = function () {
 
     //setup
     var gridContainer = document.querySelectorAll(".grid-container");
-    var battleships = [];
+    var humanBattleships = [];
+    var computerBattleships = []
     const GridSide = 10;
     const GridTotal = GridSide * GridSide;
     var playerGrid;
     var computerGrid;
+    var gamePlay = true;
 
-
+    //create grids
     drawGrid(GridSide, GridTotal);
+    resetGrids();
+
+    while (gamePlay) {
+
+    }
+
+    //create battle ships for both human and computer
     createShips();
+
+    //place battleships in each grid
+    //loop through twice for both human and computer
+    //0 for human, 1 for computer
+    for (i = 0; i < 2; i++) {
+        let grid;
+        if (i = 0) {
+            grid = playerGrid;
+        } else {
+            grid = computerGrid;
+        }
+        //get random number between 0-100
+        var placementSquare = getRandomNumber();
+        //check if cell in grid is already taken
+        checkIfCellEmpty(placementSquare, grid);
+        checkHorizontalCell(placementSquare, grid);
+        checkVerticalCell(placementSquare, grid);
+    }
+
+    //if not taken, get random number for vertical or horizontal ship placement
+    //check if cells in grid for vertical or horizontal are taken
+    //if not taken, assign grid cells to ship
+    //else (cells taken), check opposite layout
+
+    //gameplay
+    //player selects cell
+    //check if cell is assigned to ship
+    //if cell is assigned to ship, mark as hit on that ship
+    //check if ship is sunk
+    //if sunk, check if game is over
+    //else, change players
 
     function drawGrid(gridSide, gridTotal) {
         for (i = 0; i < gridContainer.length; i++) {
@@ -25,108 +64,124 @@ window.onload = function () {
         }
     }
 
-    function BattleShip(length) {
-        this.length = length;
-        this.name = length.toString();
-        this.hits = 0;
-        this.isAlive = true;
-    }
-
-    //create ship
-    function createShips() {
-        for (i = 1; i < 6; i++) {
-            let ship = new BattleShip(i);
-            battleships.push(ship)
-        }
-    }
-
-    //reset grids
-
-    //set grids
-    function resetGrids() {
-        playerGrid = new Array(100);
-        computerGrid = new Array(100);
-    }
-
-
-    //gets random number between 0-100
-    function getRandomNumber() {
-        return Math.floor(Math.random() * 100);
-    }
-    //
-    //add ships to array
-
-
-    //checks if position in given array is empty/undefined
-    function checkIfCellEmpty(number, array) {
-        if (array[number] == undefined) {
+    //returns true if horizontal neighboring cell is empty
+    function checkHorizontalCell(cell, array) {
+        if (array[cell + 1] == undefined) {
             return true;
         } else {
             return false;
         }
     }
 
-    resetGrids();
-
-    //get random number between 0-100
-    var placementSquare = getRandomNumber();
-
-    //check if ship already placed in cell with that number
-    if (checkIfCellEmpty(placementSquare, playerGrid)) {
-        //if empty, get random number (0 or 1) for layout
-        var layoutNumber = Math.floor(Math.random() * 2);
-    } else {
-        //
-    };
-
-    //get random number of 1 or 2 (horizontal or vertical placement)
-    //if 1(horizontal(+1 spaces)), check if ship already in cells to immediate right.  If so, check vertical
-    //if 2(vertical (+10 spaces)), check if ship already in cells downward, if so , check horizontal
-    //place ship starting in that square
-    //
-    //if clicked, check if box contains part of ship
-
-
-    //check if ship will go off grid
-
-    testShipObject();
-    var ship = battleships[0];
-    console.log(ship.length);
-    function testShipObject() {
-        for (i = 0; i < battleships.length; i++) {
-            console.log(battleships[i]);
+    //returns true if vertical neighboring cell is empty
+    function checkVerticalCell(cell, array) {
+        if (array[cell + 10] == undefined) {
+            return true;
+        } else {
+            return false;
         }
     }
-
-    var resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', function () {
-        testShipObject();
-    });
-
-    function markSquare() {
-        var divs = document.querySelectorAll('.grid-container div');
-        divs.forEach((div) => {
-            div.addEventListener('mouseover', function (e) {
-                e.target.setAttribute('style', 'background: black');
-            });
-        });
-    }
-
-    function gameSetup() {
-        var divs = document.querySelectorAll('.grid-container div')
-        divs.forEach((div) => {
-            div.setAttribute('style', 'background: white');
-        });
-    }
-
-    var resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', function () {
-        //gameSetup();
-    });
 
 }
 
 
+function BattleShip(length) {
+    this.length = length;
+    this.name = length.toString();
+    this.hits = 0;
+    this.isSunk = false;
+    this.cellPlacement = [];
+}
 
+//create ship
+function createShips() {
+    //loop through twice for both human and computer ships
+    for (i = 0; i < 2; i++) {
+        if (i = 0) {
+            for (j = 1; j < 6; j++) {
+                let ship = new BattleShip(j);
+                //store battleships in battleship arrays
+                humanBattleships.push(ship)
+            }
+        } else {
+            for (j = 1; j < 6; j++) {
+                let ship = new BattleShip(j);
+                //store battleships in battleship arrays
+                computerBattleships.push(ship)
+            }
+        }
+    }
+}
 
+//reset grids
 
+//set grids
+function resetGrids() {
+    playerGrid = new Array(100);
+    computerGrid = new Array(100);
+}
+
+//gets random number between 0-100
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100);
+}
+//
+//add ships to array
+
+//checks if position in given array is empty/undefined
+//returns true if empty
+function checkIfCellEmpty(number, array) {
+    if (array[number] == undefined) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+resetGrids();
+
+//check if ship already placed in cell with that number
+if (checkIfCellEmpty(placementSquare, playerGrid)) {
+    //if empty, get random number (0 or 1) for layout
+    var layoutNumber = Math.floor(Math.random() * 2);
+} else {
+    //
+};
+
+testShipObject();
+var ship = battleships[0];
+getRandomNumber();
+
+function testShipObject() {
+    for (i = 0; i < battleships.length; i++) {
+        console.log(battleships[i]);
+    }
+}
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', function () {
+    testShipObject();
+});
+
+function markSquare() {
+    var divs = document.querySelectorAll('.grid-container div');
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', function (e) {
+            e.target.setAttribute('style', 'background: black');
+        });
+    });
+}
+
+function gameSetup() {
+    var divs = document.querySelectorAll('.grid-container div')
+    divs.forEach((div) => {
+        div.setAttribute('style', 'background: white');
+    });
+}
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', function () {
+    //gameSetup();
+});
+
+}
